26383a78dcd7d70317619e0c7cdfba56e0efa769
abijith <abijithsurendran005@gmail.com>
2025-05-25 14:40:46 +0530

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,36 @@
-import express, { Request, Response } from "express"
-import shared from "@brace-for-impact/bfi-shared"
+import express, { Request, Response } from "express";
+import shared from "@brace-for-impact/bfi-shared";
 import Docker from "dockerode";
+import expressAsyncHandler from "express-async-handler";
 
-const app = express()
+const app = express();
 const docker = new Docker({ socketPath: "/var/run/docker.sock" });
 
-app.use(express.json())
-app.use(shared.middlewares.apiLogger({
-  logHttpMethod: true,
-  logRequestUrl: true,
-  logRequestBody: true,
-  logResponseTime: true,
-  logStatusCode: true,
-}))
+app.use(express.json());
+app.use(
+  shared.middlewares.apiLogger({
+    logHttpMethod: true,
+    logRequestUrl: true,
+    logRequestBody: true,
+    logResponseTime: true,
+    logStatusCode: true,
+  })
+);
 
-app.get('/api/health',async (req:Request,res:Response)=>{
-  try {
-    res
-      .status(200)
-      .json({
-        status:"true",
-        message:"Auth server is healty",
-        data: await shared.services.dockerServices.getContainerServices({docker, networkName: "bfi-infrastructure_bfi-dev-net" })
-      })
-  } catch (error) {
-    shared.middlewares.errorHandler(error,req,res)
-  }
-})
+app.get(
+  "/api/health",
+  expressAsyncHandler(async (req: Request, res: Response) => {
+    res.status(200).json({
+      status: "true",
+      message: "Auth server is healty",
+      data: await shared.services.dockerServices.getContainerServices({
+        docker,
+        networkName: "bfi-infrastructure_bfi-dev-net",
+      }),
+    });
+  })
+);
+
+app.use(shared.middlewares.errorHandler);
 
 export default app;
